359979b9ea80406fe265cd98e0bd36df1b767496
AlAbdelmegeed <AlAbdelmegeed@banquemisr.com>
2025-05-15 21:40:53 +0300

    test

--- src/main/java/org/example/springbatchpoc/config/SpringBatchConfig.java
+++ src/main/java/org/example/springbatchpoc/config/SpringBatchConfig.java
@@ -6,7 +6,10 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.explore.support.JobExplorerFactoryBean;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.dao.DefaultExecutionContextSerializer;
 import org.springframework.batch.core.repository.dao.Jackson2ExecutionContextStringSerializer;
@@ -23,7 +26,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
-
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import lombok.AllArgsConstructor;
 
 import javax.sql.DataSource;
@@ -96,19 +99,36 @@ public class SpringBatchConfig {
 
 
 	@Bean
-	public JobRepository jobRepository(DataSource dataSource) throws Exception {
+	public PlatformTransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+
+	@Bean
+	public JobRepository jobRepository(DataSource dataSource, PlatformTransactionManager transactionManager) throws Exception {
 		JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
 		factory.setDataSource(dataSource);
-		factory.setTransactionManager(new DataSourceTransactionManager(dataSource));
-
+		factory.setTransactionManager(transactionManager);
 		factory.setSerializer(new Jackson2ExecutionContextStringSerializer());
+		factory.afterPropertiesSet();
+		return factory.getObject();
+	}
 
+
+	@Bean
+	public JobExplorer jobExplorer(DataSource dataSource, PlatformTransactionManager transactionManager) throws Exception {
+		JobExplorerFactoryBean factory = new JobExplorerFactoryBean();
+		factory.setDataSource(dataSource);
+		factory.setTransactionManager(transactionManager); // ✅ This is what you're missing
 		factory.afterPropertiesSet();
 		return factory.getObject();
 	}
 
 	@Bean
-	public PlatformTransactionManager transactionManager(DataSource dataSource) {
-		return new DataSourceTransactionManager(dataSource);
+	public JobLauncher jobLauncher(JobRepository jobRepository) throws Exception {
+		SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+		jobLauncher.setJobRepository(jobRepository);
+		jobLauncher.afterPropertiesSet();
+		return jobLauncher;
 	}
+
 }
