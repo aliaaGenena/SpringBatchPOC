977524da2fef7f3ca5f1063d8c2ea95e270fbe9a
AlAbdelmegeed <AlAbdelmegeed@banquemisr.com>
2025-06-15 09:32:29 +0300

    test

--- src/main/java/org/example/springbatchpoc/config/RetryFailedJobs.java
+++ src/main/java/org/example/springbatchpoc/config/RetryFailedJobs.java
@@ -1,33 +1,33 @@
 package org.example.springbatchpoc.config;
 
 import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.batch.core.launch.JobOperator;
+
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 
 @Component
-public class AutoRecoverAndRetryFailedJobs {
+public class RetryFailedJobs {
 
     private final JobExplorer jobExplorer;
     private final JobRepository jobRepository;
     private final JobLauncher jobLauncher;
     private final JobOperator jobOperator;
 
-    public AutoRecoverAndRetryFailedJobs(JobExplorer jobExplorer,
-                                         JobRepository jobRepository,
-                                         JobRegistry jobRegistry,
-                                         JobLauncher jobLauncher,
-                                         JobOperator jobOperator) {
+    public RetryFailedJobs(JobExplorer jobExplorer,
+                           JobRepository jobRepository,
+                           JobRegistry jobRegistry,
+                           JobLauncher jobLauncher,
+                           JobOperator jobOperator) {
         this.jobExplorer = jobExplorer;
         this.jobRepository = jobRepository;
         this.jobLauncher = jobLauncher;
@@ -35,7 +35,7 @@ public class AutoRecoverAndRetryFailedJobs {
     }
 
 
-    @Scheduled(fixedRate = 60 * 60 * 1000) // every 1 hour
+  //  @Scheduled(fixedRate = 60 * 60 * 1000) // every 1 hour
     public void launchJob(){
         for (String jobName : jobExplorer.getJobNames()) {
             List<JobInstance> instances = jobExplorer.findJobInstancesByJobName(jobName, 0, 100);
@@ -63,16 +63,38 @@ public class AutoRecoverAndRetryFailedJobs {
                     break; // ✅ Skip the rest of this instance's executions
                 }
 
-                if (latestExecution.getStatus().isUnsuccessful() || status == BatchStatus.FAILED
-                        || (status == BatchStatus.STARTED
-                        && latestExecution.getEndTime() == null)){
-                       // && latestExecution.getStartTime().isBefore(LocalDateTime.now().minusHours(1)))) {
-                    try {
-                        System.out.println("Stuck execution found AND Retrying job: " + jobName + " [latestExecutionId=" + latestExecution.getId() + "]");
-                       System.out.println("Job appears stuck. Marking as FAILED.");
+                if ((status == BatchStatus.STARTED || status == BatchStatus.STARTING)
+                        && latestExecution.getEndTime() == null) {
+
+                    boolean isStepStuck = latestExecution.getStepExecutions().stream()
+                            .anyMatch(step -> step.getStatus() == BatchStatus.STARTED && step.getEndTime() == null);
+
+                    if (isStepStuck) {
+                        System.out.println("Job appears stuck. Forcing status to FAILED");
                         latestExecution.setStatus(BatchStatus.FAILED);
-                        latestExecution.setEndTime(LocalDateTime.now());
+                        latestExecution.setExitStatus(new ExitStatus("FAILED", "Execution Forced failure due to unexpected shutdown"));
+                        latestExecution.addFailureException(
+                                new UnexpectedJobExecutionException("Unexpected job execution")
+                        );
+
                         jobRepository.update(latestExecution);
+
+
+                        for (StepExecution stepExecution : latestExecution.getStepExecutions()) {
+                            if (stepExecution.getStatus() == BatchStatus.STARTED && stepExecution.getEndTime() == null) {
+                                stepExecution.setStatus(BatchStatus.FAILED);
+                                stepExecution.setExitStatus(new ExitStatus("FAILED", "Step Forced failure due to unexpected shutdown"));
+                                stepExecution.setEndTime(LocalDateTime.now()); // Don't leave it null
+                                jobRepository.update(stepExecution);
+                            }
+                        }
+                    }
+                }
+
+
+                if (latestExecution.getStatus().isUnsuccessful() || status == BatchStatus.FAILED){
+                  try {
+
                         jobOperator.restart(latestExecution.getId());
 
                     } catch (JobInstanceAlreadyCompleteException e) {
